fc5105f052a9e55ae95f9417a381a0a2c1d9d733
Anton Volkov <s22volkanto@vea.lv>
2024-03-01 15:10:41 +0200

    Added service

--- src/model/Professor.java
+++ src/model/Professor.java
@@ -1,37 +1,39 @@
 package model;
 
 public class Professor {
+	//1. variables
 	private long pID;
 	private String name;
 	private String surname;
 	private Degree profDegree;
 	
 	private static long counter = 0;
-	
+	//2. set and get
 	public long getpID() {
 		return pID;
 	}
-	public void setpID(long pID) {
+	public void setpID() {
 		this.pID = counter;
 		counter++;
 	}
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
-		if(name != null & name.matches("[A-Z]{1} [a-z]{1, 20}"))
+		if(name != null  && name.matches("[A-Z]{1}[a-z]{1,20}"))
 			this.name = name;
 		else
-			this.name = "Undefined";
+			this.name = "Undefinied";
 	}
 	public String getSurname() {
 		return surname;
 	}
 	public void setSurname(String surname) {
-		if(surname != null & surname.matches("[A-Z]{1} [a-z]{1, 20}"))
+		if(surname != null && surname.matches("[A-Z]{1}[a-z]{1,20}"))
 			this.surname = surname;
 		else
-			this.surname = "Undefined";
+			this.surname = "Undefinied";
 	}
 	public Degree getProfDegree() {
 		return profDegree;
@@ -41,7 +43,31 @@ public class Professor {
 			this.profDegree = profDegree;
 		else
 			this.profDegree = Degree.other;
-			
+	}
+		
+	
+	//3. constructors
+	public Professor() {
+		setpID();
+		setName("John");
+		setSurname("Big");
+		setProfDegree(Degree.other);
 	}
 	
+	public Professor(String name, String surname, Degree profDegree) {
+		setpID();
+		setName(name);
+		setSurname(surname);
+		setProfDegree(profDegree);
+	}
+	
+
+	//4. toString
+	@Override
+	public String toString()
+	{
+		return pID + ": " + name + " " + surname + "(" + profDegree + ")";
+	}
+	//5. other functions
+
 }
---
+++ src/sevice/MainService.java
@@ -0,0 +1,10 @@
+package sevice;
+
+public class MainService {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
